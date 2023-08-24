80e3c0b7866194378c6674d91b71d435bf92559f
Vera Deeva <verasdee@gmail.com>
2023-08-24 15:25:23 +0700

    feat: load API for select cities

--- js/script.js
+++ js/script.js
@@ -1,7 +1,55 @@
-const citySelect = document.querySelector('#city');
-const cityChoices = new Choices(citySelect, {
-    searchEnabled: false,
-    itemSelectText: '',
-});
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
 
-// console.log('citySelect', citySelect);
+
+
+const getData = async (url, cbSuccess, cbError) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        cbSuccess(data);
+    } catch (err) {
+        cbError(err)
+    }
+}
+
+const init = () => {
+    const citySelect = document.querySelector('#city');
+    const cityChoices = new Choices(citySelect, {
+        // searchEnabled: false,
+        itemSelectText: '',
+    });
+
+    getData(
+        `${API_URL}${LOCATION_URL}`,
+        (locationData) => {
+            const locations = locationData.map(location => ({
+                value: location,
+            }));
+            cityChoices.setChoices(
+                // [{ value: 'Калининград' }, { value: 'Новосибирск' }],
+                locations,
+                'value',
+                'label',
+                true)
+        },
+        (err) => {
+            console.log('err', err);
+        },
+    );
+
+}
+
+init();
+/*
+fetch(API_URL + LOCATION_URL)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log('data', data);
+    })
+    .catch((err) => {
+        console.log('err', err);
+    });
+    */
